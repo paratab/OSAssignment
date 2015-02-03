0814adb76c466c953a5334530e77980b283b20b2
paratab <iamdreammy@hotmsil.com>
2015-02-03 23:28:46 +0700

    Hash table + Array to Sort Key + List keep filename

--- index.c
+++ index.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 #include <glib.h>
 
+gint sort_strcmp(guint *a,guint *b);
+
 int main(int argc, char *argv[]){
 
 	GDir *dir = NULL;
 	const gchar *fname = NULL;
+	gchar *ctemp;
 	GString *word = g_string_new(NULL);
 	char c;
 	FILE *file;
 	GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
+	GSList *list = NULL;
+	GPtrArray *dict = g_ptr_array_new();
+	gint i = 0;
 
 	if(argc>1){
 		dir = g_dir_open(argv[1],0,NULL); // Open Directory 
 		g_chdir(argv[1]); // change dir to /data to open file
 		while((fname = g_dir_read_name(dir))!=NULL){ // Get Filename at random
 			file = fopen(fname,"r");
+
+			g_string_append(word,fname);
+			g_string_erase(word,0,4);
+			g_string_truncate(word,(word->len)-4);
+			fname = g_strdup(word->str);
+			g_string_erase(word,0,-1);
+
+
 			if(!file) {
 				printf("Open Error\n");
 				return 1;
@@ -23,16 +37,53 @@ int main(int argc, char *argv[]){
 				c = fgetc(file);
 				if(isalpha(c)){
 					g_string_append_c(word,tolower(c)); 
-				}else if((word->len)>0){
-					g_hash_table_insert(hash,word->str,(*fname));
-					printf("%s from %s\n",word->str,g_hash_table_lookup(hash,word->str));
-					g_string_assign(word,""); 
-				}				
+				}else if(word->len >0){
+					if((list = g_hash_table_lookup(hash,word->str)) == NULL){
+						list = g_slist_prepend(list,g_strdup(fname));
+						ctemp = g_strdup(word->str); // New index of word prevent changing string
+						g_hash_table_insert(hash,ctemp,list);
+						g_ptr_array_add(dict,ctemp);
+						g_string_erase(word,0,-1);	
+						i++;
+					}else{
+						if(g_strcmp0(fname,list->data)!=0) list = g_slist_prepend(list,g_strdup(fname));
+						g_hash_table_insert(hash,word->str,list);
+						g_string_erase(word,0,-1);	
+					}
+				}			
 			} while(c != EOF);
 			fclose(file);
 		} 
 		g_dir_close(dir);
-
 	}
+	//printf("\n");
+	//for( i = 0;i < (dict->len);i++){
+	//	printf("%s,",g_ptr_array_index(dict,i));
+	//}
+	//printf("\n");
+	g_ptr_array_sort(dict,(GCompareFunc)sort_strcmp);
+	//for( i = 0;i < (dict->len);i++){
+	//	printf("%s,",g_ptr_array_index(dict,i));
+	//}
+	//printf("\n");
+	printf("%d\n",dict->len);
+	/*for( i = 0;i < (dict->len);i++){
+		//printf("loop\n");
+		ctemp = g_ptr_array_index(dict,i);
+		list = g_hash_table_lookup(hash,ctemp);
+		list = g_slist_sort(list,(GCompareFunc)g_strcmp0);
+		printf("%s : %d : ",ctemp,g_slist_length(list));
+		while(g_slist_next(list) != NULL){
+			printf("%s,",list->data);
+			list = g_slist_next(list);
+		}
+		printf("%s\n",list->data);
+		g_slist_free (list);
+	}*/
+	g_hash_table_destroy (hash);
 	return 0;
+}
+
+gint sort_strcmp(guint *a,guint *b){
+	return g_strcmp0(GUINT_TO_POINTER(*a),GUINT_TO_POINTER(*b));
 }
