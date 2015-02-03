ca62d60d220e1cc066e496c114a273a99831dc6f
DreamMii <surawit_sj@localhost.localdomain>
2015-02-03 16:44:52 +0700

    Test Git With Test Hash table

--- index.c
+++ index.c
@@ -8,32 +8,31 @@ int main(int argc, char *argv[]){
 	GString *word = g_string_new(NULL);
 	char c;
 	FILE *file;
+	GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
 
 	if(argc>1){
-		dir = g_dir_open(argv[1],0,NULL);
+		dir = g_dir_open(argv[1],0,NULL); // Open Directory 
 		g_chdir(argv[1]); // change dir to /data to open file
-		while((fname = g_dir_read_name(dir))!=NULL){
-			//printf("Open : %s :",fname);
+		while((fname = g_dir_read_name(dir))!=NULL){ // Get Filename at random
 			file = fopen(fname,"r");
 			if(!file) {
-				//printf("Open Error\n");
+				printf("Open Error\n");
 				return 1;
 			}
-			//printf("Done\n");
 			do{
 				c = fgetc(file);
 				if(isalpha(c)){
-					g_string_append_c(word,tolower(c));
-				}else{
-					//Do String here
-					//printf("%s ",word->str);
-					g_string_assign (word,"");
+					g_string_append_c(word,tolower(c)); 
+				}else if((word->len)>0){
+					g_hash_table_insert(hash,word->str,(*fname));
+					printf("%s from %s\n",word->str,g_hash_table_lookup(hash,word->str));
+					g_string_assign(word,""); 
 				}				
 			} while(c != EOF);
 			fclose(file);
 		} 
 		g_dir_close(dir);
+
 	}
 	return 0;
-
 }
