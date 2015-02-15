213838cd41bdc6307a55efe9ad1d57fcbbad651d
paratab <iamdreammy@hotmsil.com>
2015-02-15 14:01:30 +0700

    List to array

--- index.c
+++ index.c
@@ -3,7 +3,7 @@
 #include <glib.h>
 
 gint sort_strcmp(guint *a,guint *b);
-gint sort_intcmp(gconstpointer a,gconstpointer b);
+gint sort_intcmp(guint *a,guint *b);
 
 int main(int argc, char *argv[]){
 
@@ -15,9 +15,9 @@ int main(int argc, char *argv[]){
 	char c;
 	FILE *file;
 	GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
-	GSList *list = NULL;
 	GPtrArray *dict = g_ptr_array_new();
-	gint i = 0;
+	GPtrArray *farray;
+	gint i = 0,j=0;
 
 	if(argc>1){
 		dir = g_dir_open(argv[1],0,NULL); // Open Directory 
@@ -25,33 +25,35 @@ int main(int argc, char *argv[]){
 		while((fname = g_dir_read_name(dir))!=NULL){ // Get Filename at random
 			file = fopen(fname,"r");
 
-			g_string_append(word,fname);
-			g_string_erase(word,0,4);
-			g_string_truncate(word,(word->len)-4);
+			for(i=0;fname[i]!='\0';i++){
+				if(isdigit(fname[i])) g_string_append_c(word,fname[i]); 
+			}
 			fname = g_strdup(word->str);
-			g_string_erase(word,0,-1);
 			ftemp = g_strdup(fname);
+			g_string_erase(word,0,-1);
 
 
 			if(!file) {
 				printf("Open Error\n");
-				return 1;
+				exit(1);
 			}
 			do{
 				c = fgetc(file);
 				if(isalpha(c)){
 					g_string_append_c(word,tolower(c)); 
 				}else if(word->len >0){
-					if((list = g_hash_table_lookup(hash,word->str)) == NULL){
-						list = g_slist_prepend(list,ftemp);
+					if((farray = g_hash_table_lookup(hash,word->str)) == NULL){
+						farray = g_ptr_array_new();
+						g_ptr_array_add(farray,ftemp);
 						ctemp = g_strdup(word->str); // New index of word prevent changing string
-						g_hash_table_insert(hash,ctemp,list);
+						g_hash_table_insert(hash,ctemp,farray);
 						g_ptr_array_add(dict,ctemp);
 						g_string_erase(word,0,-1);	
-						i++;
 					}else{
-						if(g_strcmp0(fname,list->data)!=0) list = g_slist_prepend(list,ftemp);
-						g_hash_table_insert(hash,word->str,list);
+						if(g_strcmp0(fname,g_ptr_array_index(farray,farray->len-1))!=0){
+							g_ptr_array_add(farray,ftemp);
+						}
+						g_hash_table_insert(hash,word->str,farray);
 						g_string_erase(word,0,-1);	
 					}
 				}			
@@ -65,18 +67,17 @@ int main(int argc, char *argv[]){
 
 	g_chdir("..");
 	file = fopen(output,"w");
-	fprintf(file, "%d\n",dict->len);
+	fprintf(file,"%d\n",dict->len);
 	for( i = 0;i < (dict->len);i++){
 		ctemp = g_ptr_array_index(dict,i);
-		list = g_hash_table_lookup(hash,ctemp);
-		list = g_slist_sort(list,(GCompareFunc)sort_intcmp);
-		fprintf(file,"%s:%d:",ctemp,g_slist_length(list));
-		while(g_slist_next(list) != NULL){
-			fprintf(file,"%s,",list->data);
-			list = g_slist_next(list);
+		farray = g_hash_table_lookup(hash,ctemp);
+		g_ptr_array_sort(farray,(GCompareFunc)sort_intcmp);
+		fprintf(file,"%s:%d:",ctemp,farray->len);
+		for(j = 0;j<(farray->len-1);j++){
+			fprintf(file,"%s,",g_ptr_array_index(farray,j));
 		}
-		fprintf(file,"%s\n",list->data);
-		g_slist_free (list);
+		fprintf(file,"%s\n",g_ptr_array_index(farray,farray->len-1));
+		g_ptr_array_free(farray,TRUE);
 	}
 	g_hash_table_destroy (hash);
 	fclose(file);
@@ -87,6 +88,6 @@ int main(int argc, char *argv[]){
 gint sort_strcmp(guint *a,guint *b){
 	return g_strcmp0(GUINT_TO_POINTER(*a),GUINT_TO_POINTER(*b));
 }
-gint sort_intcmp(gconstpointer a,gconstpointer b){
-	 return atoi(a) - atoi(b);
+gint sort_intcmp(guint *a,guint *b){
+	 return atoi(GUINT_TO_POINTER(*a)) - atoi(GUINT_TO_POINTER(*b));
 }
